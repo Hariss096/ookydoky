5942acf7833ece5ee8be400feb87b45c9e689b41
Haris <hariss096@gmail.com>
2018-04-08 05:38:25 -0700

    ookydoky-ecommerce website first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAuodqI3zlQSPfInOHB3FdVRboRTH4Djqo",
+    authDomain: "ookydoky-c3538.firebaseapp.com",
+    databaseURL: "https://ookydoky-c3538.firebaseio.com",
+    projectId: "ookydoky-c3538",
+    storageBucket: "ookydoky-c3538.appspot.com",
+    messagingSenderId: "455887172901"
+  }  
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAuodqI3zlQSPfInOHB3FdVRboRTH4Djqo",
+    authDomain: "ookydoky-c3538.firebaseapp.com",
+    databaseURL: "https://ookydoky-c3538.firebaseio.com",
+    projectId: "ookydoky-c3538",
+    storageBucket: "ookydoky-c3538.appspot.com",
+    messagingSenderId: "455887172901"
+  }
 };
